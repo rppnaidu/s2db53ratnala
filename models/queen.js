d182f5db919bbb8db3f33284e45d5ac855471e83
rppnaidu <S547053@NWMISSOURI.EDU>
2022-04-28 10:50:12 -0500

    final commit

--- models/queen.js
+++ models/queen.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose")
 const queenSchema = mongoose.Schema({
     queen_name: String,
     queen_country: String,
-    networth: Number
+    networth: { 
+        type: Number, 
+        min:1500000, 
+        max:2900000
+    }
 })
 module.exports = mongoose.model("Queen", queenSchema)
--- routes/queens.js
+++ routes/queens.js
@@ -8,16 +8,23 @@ router.get('/', function(req, res, next) {
   res.render('queens', { title: 'Search Results' });
 });
 */
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET costumes */
 router.get('/', queen_controller.queen_view_all_Page );
 /* GET detail queen page */
 router.get('/detail', queen_controller.queen_view_one_Page);
 /* GET create queen page */
-router.get('/create', queen_controller.queen_create_Page);
+router.get('/create', secured, queen_controller.queen_create_Page);
 /* GET create update page */
-router.get('/update', queen_controller.queen_update_Page);
+router.get('/update', secured, queen_controller.queen_update_Page);
 /* GET delete queen page */
-router.get('/delete', queen_controller.queen_delete_Page);
+router.get('/delete', secured, queen_controller.queen_delete_Page);
 
 module.exports = router;
